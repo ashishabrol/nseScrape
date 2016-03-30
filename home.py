c346d467716bccd801e2525380a15646d68eba32
root <ashish.abrol@gmail.com>
2016-03-30 12:33:34 -0400

    added query property to orderbook

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ from lxml.etree import tostring
 from lxml.builder import E
 from mako.template import Template
 
-from bottle import route, run, static_file,template
+from bottle import route, run, static_file,template,request
 
 @route('/home')
 def home():
@@ -38,5 +38,51 @@ def orderbook(month):
     htmlfile.write(mytemplate.render(cursor=cursor))
     htmlfile.close()
     return static_file("tmplhtml_orderbook.html",root="")
+
+
+
+@route('/orderbook')
+def home():
+        args=request.query.decode()
+        str1="select * from niftyfuture where "
+        for key in args.iterkeys():
+            if key=="quantity":
+                if args[key].startswith('>') or args[key].startswith('<') or args[key].startswith('!'):
+                    str1+="(mBuyQuantity1{0} or ".format(args[key])
+                    str1+="mBuyQuantity2{0} or ".format(args[key])
+                    str1+="mBuyQuantity3{0} or ".format(args[key])
+                    str1+="mBuyQuantity4{0} or ".format(args[key])
+                    str1+="mBuyQuantity5{0} or ".format(args[key])
+
+                    str1+="mSellQuantity1{0} or ".format(args[key])
+                    str1+="mSellQuantity2{0} or ".format(args[key])
+                    str1+="mSellQuantity3{0} or ".format(args[key])
+                    str1+="mSellQuantity4{0} or ".format(args[key])
+                    str1+="mSellQuantity5{0}) and ".format(args[key])
+                    
+            if key=="date":
+                if args[key].startswith('>') or args[key].startswith('<') or args[key].startswith('!'):
+                    str1+="date1{0}date(\'{1}\') and ".format(args[key][0],args[key][1:])
+                else:
+                    str1+="date1=date(\'{0}\') and ".format(args[key])
+
+        str1=(str1.strip()).rsplit(' ', 1)[0]
+        str1=str1.replace("date1", "date(unixtime,'unixepoch')")
+        q=str1
+        db=sqlite3.connect("test.db",isolation_level=None)
+        cursor=db.cursor()
+        cursor.execute(q)
+        htmlfile=open("tmplhtml.html","w")
+        mytemplate=Template(filename='tmpl12.txt')
+        htmlfile.write(mytemplate.render(cursor=cursor))
+	htmlfile.close()
+        htmlfile=open("tmplhtml.html","r")
+        var=htmlfile.read()
+	htmlfile.close()
+	return var
+        return static_file("tmplhtml.html",root="")
+
+
+
 
 run(host='vps70976.vps.ovh.ca',port=8080, debug=True)
